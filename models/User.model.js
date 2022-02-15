10931b373b1e939b5c4ad1fc0172ef65569eda9a
Marcelus <kz_@hotmail.es>
2022-02-15 18:29:49 +0100

    No env please

--- models/User.model.js
+++ models/User.model.js
@@ -22,6 +22,10 @@ const userSchema = new Schema(
     passwordHash: {
       type: String,
       required: [true, "Password is required."]
+    },
+    imgUrl: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/456/456212.png",
     }
   },
   {
